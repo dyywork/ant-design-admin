7df65d2ded1fc31f71521ed5751ac16ac8bb9857
dingyongya <yahya_dyy@163.com>
2022-02-15 10:00:15 +0800

    add: 添加commitlint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
-import App from './App.vue'
-import 'ant-design-vue/dist/antd.css'
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(Antd).mount('#app')
+createApp(App).use(Antd).mount('#app');
