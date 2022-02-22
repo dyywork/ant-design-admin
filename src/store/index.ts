2a45070553c01cc0240c0257968ab0abd418b716
dingyongya <yahya_dyy@163.com>
2022-02-22 10:16:28 +0800

    change: 格式调整
    格式

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
 import { createStore } from 'vuex';
 import getters from './getters';
 
-const modulesFiles = import.meta.globEager("./modules/**/*.ts")
-const modules = Object.keys(modulesFiles).reduce((modules: any, modulePath: string) => {
-  const moduleName = modulePath.replace(/(.*\/)*([^.]+).*/ig,"$2")
-  const value = modulesFiles[modulePath]
-  modules[moduleName] = value.default
-  return modules
-}, {})
+const modulesFiles = import.meta.globEager('./modules/**/*.ts');
+const modules = Object.keys(modulesFiles).reduce(
+  (modules: any, modulePath: string) => {
+    const moduleName = modulePath.replace(/(.*\/)*([^.]+).*/gi, '$2');
+    const value = modulesFiles[modulePath];
+    modules[moduleName] = value.default;
+    return modules;
+  },
+  {}
+);
 
 export default createStore({
   modules,
