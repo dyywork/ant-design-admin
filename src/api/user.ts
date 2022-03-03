c57dd42f3e62edd7e28b173bd70c4a726d9e51e5
dingyongya <yahya_dyy@163.com>
2022-03-03 13:36:43 +0800

    add: mock
    格式调整

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,8 @@
-import request from '@/utils/request'
+import request from '@/utils/request';
 
 export function getMenuList() {
-    return request({
-        url: '/api/menu/list',
-        method: 'post'
-    })
+  return request({
+    url: '/api/menu/list',
+    method: 'post',
+  });
 }
-
