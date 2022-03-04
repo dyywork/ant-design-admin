3a8bec031b92f27309e3f94fa69f62e1c426e32b
dingyongya <yahya_dyy@163.com>
2022-03-04 10:55:59 +0800

    change: 格式
    格式修改

--- mock/user.ts
+++ mock/user.ts
@@ -1,9 +1,39 @@
-import Mock from 'mockjs'
+import Mock from 'mockjs';
 
 Mock.mock('/api/menu/list', 'post', () => {
-    return  [
-                {
-                    name: 1231
-                }
-            ]
-})
+  return [
+    {
+      path: '/',
+      redirect: '/home',
+      component: '',
+      meta: { title: '内容', icon: 'el-icon-location' },
+      children: [
+        {
+          path: '/home',
+          name: 'Home',
+          component: '@/views/Home/Home.vue',
+          meta: { title: 'Home' },
+        },
+        {
+          path: '/table',
+          name: 'Table',
+          component: '@/views/Table/Table.vue',
+          meta: { title: 'Table' },
+        },
+      ],
+    },
+    {
+      path: '/center',
+      component: '',
+      meta: { title: '个人中心', icon: 'el-icon-location' },
+      children: [
+        {
+          path: '/center',
+          name: 'Center',
+          component: '@/views/Center/Center.vue',
+          meta: { title: 'Center' },
+        },
+      ],
+    },
+  ];
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const {resolve} = require('path')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+const { resolve } = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,9 +14,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
+        rewrite: path => path.replace(/^\/api/, '/api'),
       },
-    
-    }
-  }
-})
+    },
+  },
+});
